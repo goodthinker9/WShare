056916f39eaa8d805f18fad882a683e999f85063
goodthinker9 <kalidmmohammed8@gmail.com>
2026-09-25 15:58:15 +0300

    Neon SSL connection

--- server/config/database.js
+++ server/config/database.js
@@ -25,6 +25,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
